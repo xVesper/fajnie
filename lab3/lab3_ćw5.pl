c9c6f4bc80363ad54c57e0b968813150bc45523e
xVesper <73543770+xVesper@users.noreply.github.com>
2022-03-07 15:13:45 +0100

    Add files via upload

---
+++ lab3/lab3_ćw5.pl
@@ -0,0 +1,15 @@
+% Rekurencja
+% Silnia: 0!=1,n!=n*(n-1)!
+
+%silnia(N,S)
+%S=N!
+
+%warunek zakoñczeia rekurencji
+ silnia(0,1).
+
+%rekurencja
+silnia(N,S):-
+    N>0,
+    N1 is N-1,
+    silnia(N1,S1),
+    S is N*S1.
